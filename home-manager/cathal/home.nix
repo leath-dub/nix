676b71e0ddf91aba3a682a7ac3987099180010ab
leath-dub <fierceinbattle@gmail.com>
2023-10-15 18:58:51 +0100

    added nix.conf

--- home-manager/cathal/home.nix
+++ home-manager/cathal/home.nix
@@ -1,14 +1,28 @@
 { username, config, pkgs, ... }:
 
 {
+
   home = {
     inherit username;
-    homeDirectory = /home/${username};
+    homeDirectory = "/home/${username}";
     stateVersion = "23.11";
   };
+
+  home.file = {
+    nix-conf = {
+      enable = true;
+      source = ./system/nix.conf;
+      target = "./.config/nix/nix.conf";
+    };
+  };
+
   programs.git = {
     enable = true;
     userName = "leath-dub";
     userEmail = "fierceinbattle@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      color.ui = "auto";
+    }; 
   };
 }
