856dc45082a54ad9ced54b254ffed1154fa9e8eb
leath-dub <fierceinbattle@gmail.com>
2023-10-15 19:01:45 +0100

    changed path to be relative of git repo

--- home-manager/cathal/home.nix
+++ home-manager/cathal/home.nix
@@ -11,7 +11,7 @@
   home.file = {
     nix-conf = {
       enable = true;
-      source = ./system/nix.conf;
+      source = ../../system/nix.conf;
       target = "./.config/nix/nix.conf";
     };
   };
