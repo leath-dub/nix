8c7f213eccde78e31a478043011a0a686622a9ce
cathal <cathal@localhost.my.domain>
2023-10-15 17:17:36 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      username = "cathal";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      homeConfigurations = {
+        "${username}" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            {
+              home = {
+                inherit username;
+                homeDirectory = /home/${username};
+                stateVersion = "23.11-pre";
+              };
+              programs.home-manager.enable = true;
+            }
+          ];
+          extraSpecialArgs = {
+            inherit inputs system;
+          };
+        };
+      };
+    });
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "cathal";
+  home.homeDirectory = "/home/cathal";
+  home.stateVersion = "23.11-pre";
+}
